7a544c6efa5aab32c363f8ddc40b21b946798577
sanjaykakkar222 <47442582+sanjaykakkar222@users.noreply.github.com>
2019-02-20 15:27:28 +0530

    Update external.js

--- Tax/external.js
+++ Tax/external.js
@@ -1,40 +1,50 @@
-let a = [
-    { type : 'A',
-    percentage:10   
+let a = [ //Array a
+    {
+        type: 'A',
+        percentage: 10
     },
-    { type : 'B',
-      percentage:5   
+    {
+        type: 'B',
+        percentage: 5
     },
-    { type : 'C',
-      percentage:20   
+    {
+        type: 'C',
+        percentage: 20
     },
-    { type : 'D',
-      percentage:70   
+    {
+        type: 'D',
+        percentage: 70
     },
-    { type : 'E',
-      percentage:2   
+    {
+        type: 'E',
+        percentage: 2
     }
-  ]
-  
-  function calculateTax(type='D', amount=0){
-      var percent= 0;
-      
-      for(let i=1;i<5;i++){
-          
-          
-          if(type == a[i].type ){
-               
-               percent = a[i].percentage;
-          }
-      }
-      var amountDeduct =amount*(percent/100);
-      var finalresult = { "type" : type,
-                    "Total amount": amount,
-                      "percent":percent,
-                      "amount deducted":amountDeduct,
-                   "Amount after deduction":amount - amountDeduct };
-  
-          return finalresult;
-  
-  
-  }
+]
+
+function calculateTax(type = 'D', amount = 0) { //calculate Tax
+    var percent = 0;
+
+    for (let i = 1; i < 5; i++) {
+
+
+        if (type == a[i].type) {
+
+            percent = a[i].percentage;
+        }
+    }
+    var amountDeduct = amount * (percent / 100);
+    calculategst
+    var finalresult = {
+        "type": type,
+        "Total amount": amount,
+        "percent": percent,
+        "amount deducted": amountDeduct,
+        "Amount after deduction": amount - amountDeduct
+    };
+
+    return finalresult; //final result will return 5 parameters
+
+
+}
+
+console.log('C', 100);
